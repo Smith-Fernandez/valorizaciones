a5673076287c93e4372a1330c5111a3216499605
Mcubal2794 <angel2794@hotmail.es>
2021-10-13 11:18:36 -0500

    Cambio conexion

--- Valorizaciones/Datos/Conexion.cs
+++ Valorizaciones/Datos/Conexion.cs
@@ -12,13 +12,14 @@ namespace Valorizaciones.Datos
 
         protected void Conectar()
         {
+            cnn = new SqlConnection(@"Data Source=DESKTOP-FR904MP\SQLEXPRESS;Integrated security=True; Initial Catalog=valorizaciones;");
+            // cnn = new SqlConnection("Data Source=(local);User Id =sa;Password=1nf0rm@t1c@ ;Initial Catalog=valorizaciones;");
+            // cnn = new SqlConnection(@"Data Source=DESKTOP-SV0VCFQ\SQLEXPRESS;Integrated Security=True;Initial Catalog=valorizaciones;");
+
+            cnn.Open();
             try
             {
-                  //cnn = new SqlConnection("Data Source=(local);Integrated security=SSPI; Initial Catalog=valorizaciones;");
-                //cnn = new SqlConnection("Data Source=(local);User Id =sa;Password=1nf0rm@t1c@ ;Initial Catalog=valorizaciones;");
-                 cnn = new SqlConnection(@"Data Source=DESKTOP-SV0VCFQ\SQLEXPRESS;Integrated Security=True;Initial Catalog=valorizaciones;");
-
-                cnn.Open();
+                
             }
             catch(Exception e)
             {
